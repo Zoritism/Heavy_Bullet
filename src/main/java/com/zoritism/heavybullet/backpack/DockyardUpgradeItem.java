7c77cd4646e2177cca53ec2f8653850ef8ef320a
Maksym Makarenko <startlecrow@gmail.com>
2025-06-29 12:19:08 +0200

    Make DockyardUpgradeItem.TYPE public and add upgrade tag
    Changed the visibility of DockyardUpgradeItem.TYPE to public for external access. Added a new upgrade.json tag file to register 'heavybullet:dockyard_upgrade' for sophisticatedbackpacks. Also reordered imports in DockyardUpgradeWrapper for consistency.

--- src/main/java/com/zoritism/heavybullet/backpack/DockyardUpgradeItem.java
+++ src/main/java/com/zoritism/heavybullet/backpack/DockyardUpgradeItem.java
@@ -3,12 +3,11 @@ package com.zoritism.heavybullet.backpack;
 import net.p3pp3rf1y.sophisticatedcore.upgrades.IUpgradeCountLimitConfig;
 import net.p3pp3rf1y.sophisticatedcore.upgrades.UpgradeItemBase;
 import net.p3pp3rf1y.sophisticatedcore.upgrades.UpgradeType;
-
 import java.util.Collections;
 import java.util.List;
 
 public class DockyardUpgradeItem extends UpgradeItemBase<DockyardUpgradeWrapper> {
-    private static final UpgradeType<DockyardUpgradeWrapper> TYPE = new UpgradeType<>(DockyardUpgradeWrapper::new);
+    public static final UpgradeType<DockyardUpgradeWrapper> TYPE = new UpgradeType<>(DockyardUpgradeWrapper::new);
 
     public DockyardUpgradeItem(IUpgradeCountLimitConfig config) {
         super(config);
--- src/main/java/com/zoritism/heavybullet/backpack/DockyardUpgradeWrapper.java
+++ src/main/java/com/zoritism/heavybullet/backpack/DockyardUpgradeWrapper.java
@@ -1,9 +1,8 @@
 package com.zoritism.heavybullet.backpack;
 
-import net.minecraft.world.item.ItemStack;
 import net.p3pp3rf1y.sophisticatedcore.api.IStorageWrapper;
 import net.p3pp3rf1y.sophisticatedcore.upgrades.UpgradeWrapperBase;
-
+import net.minecraft.world.item.ItemStack;
 import java.util.function.Consumer;
 
 public class DockyardUpgradeWrapper extends UpgradeWrapperBase<DockyardUpgradeWrapper, DockyardUpgradeItem> {
