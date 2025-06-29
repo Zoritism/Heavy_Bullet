2479128749bc7869c54f478887ac40f97782e005
Maksym Makarenko <startlecrow@gmail.com>
2025-06-29 11:33:35 +0200

    Refactor DockyardUpgradeItem and upgrade registration
    Updated DockyardUpgradeItem and related registration to use generic UpgradeType<?> and revised IUpgradeCountLimitConfig methods for compatibility with new API. This improves type safety and aligns with updated sophisticatedcore interfaces.

--- src/main/java/com/zoritism/heavybullet/backpack/DockyardUpgradeItem.java
+++ src/main/java/com/zoritism/heavybullet/backpack/DockyardUpgradeItem.java
@@ -1,28 +1,26 @@
 package com.zoritism.heavybullet.backpack;
 
-import net.minecraft.world.item.Item;
 import net.p3pp3rf1y.sophisticatedcore.upgrades.IUpgradeCountLimitConfig;
 import net.p3pp3rf1y.sophisticatedcore.upgrades.UpgradeItemBase;
 import net.p3pp3rf1y.sophisticatedcore.upgrades.UpgradeType;
-
 import java.util.Collections;
 import java.util.List;
 
 public class DockyardUpgradeItem extends UpgradeItemBase {
-    private final UpgradeType upgradeType;
+    private final UpgradeType<?> upgradeType;
 
-    public DockyardUpgradeItem(IUpgradeCountLimitConfig config, UpgradeType upgradeType) {
+    public DockyardUpgradeItem(IUpgradeCountLimitConfig config, UpgradeType<?> upgradeType) {
         super(config);
         this.upgradeType = upgradeType;
     }
 
     @Override
-    public UpgradeType getType() {
+    public UpgradeType<?> getType() {
         return upgradeType;
     }
 
     @Override
-    public List<UpgradeType> getUpgradeConflicts() {
+    public List<UpgradeType<?>> getUpgradeConflicts() {
         return Collections.emptyList();
     }
 }
