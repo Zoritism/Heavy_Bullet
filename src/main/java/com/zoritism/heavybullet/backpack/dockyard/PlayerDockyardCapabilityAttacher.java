d187e6bd8c295be84678296b3ac7e1be94f44f24
Maksym Makarenko <startlecrow@gmail.com>
2025-07-01 23:45:25 +0200

    Create PlayerDockyardCapabilityAttacher.java

---
+++ src/main/java/com/zoritism/heavybullet/backpack/dockyard/PlayerDockyardCapabilityAttacher.java
@@ -0,0 +1,21 @@
+package com.zoritism.heavybullet.backpack.dockyard;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber
+public class PlayerDockyardCapabilityAttacher {
+    @SubscribeEvent
+    public static void attach(AttachCapabilitiesEvent<Entity> event) {
+        if (event.getObject() instanceof Player) {
+            event.addCapability(
+                    new ResourceLocation("heavybullet", "dockyard"),
+                    new PlayerDockyardDataProvider()
+            );
+        }
+    }
+}
