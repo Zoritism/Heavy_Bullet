7d4803908cc3ba71fd48b9de5681b3f3f933b0d2
Maksym Makarenko <startlecrow@gmail.com>
2025-07-02 23:45:07 +0200

    Update DockyardUpgradeContainer.java

--- src/main/java/com/zoritism/heavybullet/backpack/dockyard/DockyardUpgradeContainer.java
+++ src/main/java/com/zoritism/heavybullet/backpack/dockyard/DockyardUpgradeContainer.java
@@ -4,7 +4,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.chunk.LevelChunk;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -16,7 +15,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.lang.reflect.Field;
-import java.util.List;
 import java.util.Map;
 
 public class DockyardUpgradeContainer extends UpgradeContainerBase<DockyardUpgradeWrapper, DockyardUpgradeContainer> {
